2a07877007c02ae31aed79b1f46e6d8573536bd2
mealilo <90289295+mealilo@users.noreply.github.com>
2022-02-15 20:46:01 -0700

    repository and pagniation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Mission7.Models;
+using Mission7.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -11,27 +12,33 @@ namespace Mission7.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private IBookRepository repo;
+
+        public IActionResult Index(int pageNum = 1)
         {
-            _logger = logger;
+
+            int pageSize = 5;
+
+            var data = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(p => p.Title)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+
+            };
+
+            return View(data);
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
---
+++ Models/BookstoreContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Mission7.Models
+{
+    public partial class BookstoreContext : DbContext
+    {
+        public BookstoreContext()
+        {
+        }
+
+        public BookstoreContext(DbContextOptions<BookstoreContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlite("Data Source = Bookstore.sqlite");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasIndex(e => e.BookId, "IX_Books_BookID")
+                    .IsUnique();
+
+                entity.Property(e => e.BookId).HasColumnName("BookID");
+
+                entity.Property(e => e.Author).IsRequired();
+
+                entity.Property(e => e.Category).IsRequired();
+
+                entity.Property(e => e.Classification).IsRequired();
+
+                entity.Property(e => e.Isbn)
+                    .IsRequired()
+                    .HasColumnName("ISBN");
+
+                entity.Property(e => e.Publisher).IsRequired();
+
+                entity.Property(e => e.Title).IsRequired();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
