1abc6c4531de9f941148e8477819f488ec663c9f
mealilo <90289295+mealilo@users.noreply.github.com>
2022-02-23 18:19:24 -0700

    cart model

---
+++ Models/Cart.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission7.Models
+{
+
+   
+    public class Cart
+    {
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+        public void AddItem(Book book, int qty)
+        {
+            ShoppingCartItem Line = Items.Where(p => p.Book.Isbn == book.Isbn).FirstOrDefault();
+
+
+
+            if (Line == null)
+            {
+                Items.Add(new ShoppingCartItem{
+                    Book = book,
+                    Quantity = qty,
+                    Cost = book.Price
+                });
+            }
+
+            else
+            {
+                Line.Quantity += qty;
+                Line.Cost += book.Price;
+            }
+        }
+
+        public double CalculateSubTotal()
+        {
+            double sum = Items.Sum(x => x.Cost);
+
+            return sum;
+        }
+
+        public int CalculateAmountBooks()
+        {
+            int sum = Items.Sum(x => x.Quantity);
+
+            return sum;
+        }
+    }
+
+        public class ShoppingCartItem
+        {
+            public int LineID { get; set; }
+            public Book Book { get; set; }
+
+        public int Quantity { get; set; }
+        public double Cost { get; set; }
+
+    }
+
+}
